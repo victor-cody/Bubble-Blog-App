d0d4e955df0596078a63f0af49e3720c7d68b242
Victor-Cody <vickyikechukwu13@gmail.com>
2021-01-20 00:08:56 +0100

    Restructured our file structure . and converted our application structure into a package

---
+++ flaskblog/models.py
@@ -0,0 +1,26 @@
+from datetime import datetime
+from flaskblog import db
+
+class User(db.Model):
+    ''''Database Table to store user's data'''
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(20), unique = True, nullable= False)
+    email = db.Column(db.String(100), unique = True, nullable= False)
+    image_file = db.Column(db.String(20), nullable= False, default = 'default.png')
+    password = db.Column(db.String(60), nullable= False)
+    posts = db.relationship('Post',backref= 'author',lazy = True)
+
+    def __repr__(self):
+        return f"User('{self.username}', '{self.email}', '{self.image_file}', '{self.image_file}')"
+
+class Post(db.Model):
+    ''''Database Table to store user's Blog Posts'''
+    id = db.Column(db.Integer, primary_key=True)
+    # author = db.Column(db.String(20), unique = True, nullable= False) 
+    title = db.Column(db.String(80), nullable= False)
+    content = db.Column(db.Text, nullable = False)
+    date_posted = db.Column(db.DateTime, nullable= False, default = datetime.utcnow)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable= False)
+
+    def __repr__(self):
+        return f"User('{self.title}', '{self.date_posted}')"
